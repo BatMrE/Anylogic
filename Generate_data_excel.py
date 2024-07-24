f72d3cd7e8d0a383b5069618c2f0ca4c63e42402
BatMrE <48859022+BatMrE@users.noreply.github.com>
2024-07-24 00:07:43 -0400

    Update Generate_data_excel.py

--- Generate_data_excel.py
+++ Generate_data_excel.py
@@ -2,3 +2,13 @@
 
 excelOutputFile.writeDataSet(datasetXSamples, 1, 2, 1);
 excelOutputFile.writeDataSet(datasetYSamples, 1, 2, 3);
+
+
+# Multiple cols
+
+excelOutputFile.setCellValue("X", 1, 1, 2);
+excelOutputFile.writeDataSet(datasetXSamples, 1, 2, 1);
+excelOutputFile.setCellValue("Y", 1, 1, 4);
+excelOutputFile.writeDataSet(datasetYSamples, 1, 2, 3);
+excelOutputFile.setCellValue("fcPrevTime", 1, 1, 6);
+excelOutputFile.writeDataSet(dataset_fcPrevTime, 1, 2, 5);
